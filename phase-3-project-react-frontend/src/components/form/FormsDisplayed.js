0c0ec886f7a02bd906f4f3d53fe3208a68a7fad9
Brian Wangora <brianwangora@gmail.com>
2022-09-13 02:04:51 +0300

    User form configured and working

--- phase-3-project-react-frontend/src/components/form/FormsDisplayed.js
+++ phase-3-project-react-frontend/src/components/form/FormsDisplayed.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Header from "../header/Header";
+import UserFormDefined from "./UserFormDefined";
+import ReviewFormDefined from "./ReviewFormDefined";
 
 export default function FormsDisplayed() {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+    const [id, setId] = useState("")
+
+    function handleDarkModeClick() {
+        setIsDarkMode((isDarkMode) => !isDarkMode);
+    }
+
+    const userIdFetcher = () => {
+        fetch("http://localhost:9292/users")
+            .then((response) => response.json())
+            .then((user) => {
+                setId(user[user.length - 1].id)
+            })
+    }
+
+    useEffect(
+        userIdFetcher, []
+    )
+
     return(
-        <p>Hello</p>
+        <div className="comtainer">
+            <div className={"App " + (isDarkMode ? "dark" : "light")}>
+                <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick}/>
+                <p>First add your name to the system in order to get an ID number which will be useful when filling out your review.</p>
+                <p>Also make sure you remember the restaurant's ID number.</p>
+                <UserFormDefined/>
+                <p>Your ID number is {id}.</p>
+                <ReviewFormDefined/>
+            </div>
+        </div>
     )
 }
